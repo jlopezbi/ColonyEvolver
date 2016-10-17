31c23f527965026f3d608f7606633f5be5739ffb
JOSHUA_LOPEZ_BINDER <joshlobinder@gmail.com>
2016-10-17 10:44:29 -0700

    added test for plant bbox

--- test_plant.py
+++ test_plant.py
@@ -1,11 +1,12 @@
 import unittest,imp
+import numpy as np
 import mathutils
 import plant
 imp.reload(plant)
 
 class StubbedParticle(object):
     def __init__(self,location=(0.0,0.0,0.0),radius=2.0):
-        self.position = mathutils.Vector(location)
+        self.position = np.array(location)
         self.radius = radius
 
 class PlantTestCase(unittest.TestCase):
@@ -23,6 +24,16 @@ class PlantTestCase(unittest.TestCase):
         self.assertTrue(self.plant.number_of_elements(),2)
         self.plant.show()
 
+    def test_updates_bbox_when_node_added(self):
+        new_node = plant.Node(0,(1,1,1))    
+        np.testing.assert_equal(self.plant.bbox_lower,(0,0,0))
+        np.testing.assert_equal(self.plant.bbox_upper,(0,0,0))
+        self.plant._add_node(new_node)
+        np.testing.assert_equal(self.plant.bbox_upper,(1,1,1))
+        new_node = plant.Node(0,(-1.,10.,1.0))    
+        self.plant._add_node(new_node)
+        np.testing.assert_equal(self.plant.bbox_lower,(-1.,0.,0.))
+        np.testing.assert_equal(self.plant.bbox_upper,(1.,10.,1.))
 
 if __name__=="__main__":
     unittest.main(exit=False)
