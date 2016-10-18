5875f1909a1024ec469efe6743b084a50c9dd50c
JOSHUA_LOPEZ_BINDER <joshlobinder@gmail.com>
2016-10-18 11:30:53 -0700

    changed plant to add new nodes at average direction between internode direction and sphere direction

--- plant.py
+++ plant.py
@@ -4,6 +4,11 @@ import imp
 import numpy as np
 import mesh_helpers
 imp.reload(mesh_helpers)
+#TODO: 
+# - clean up this module
+#probably should be a base-class for plant upon which various growth behaviors are added
+# - add plants that have constant branching direction
+# - plants that have different types of nodes
 
 '''
 notes for development:
@@ -12,6 +17,15 @@ custom attributes on a a bmesh vertex:
     and
     http://blender.stackexchange.com/questions/8992/python-api-custom-properties-for-vertices-edges-faces
 '''
+
+def get_average_vector(vectors):
+    '''
+    vectors must be tuple of np.array() with only one dimension
+    all vectors must be same size
+    '''
+    c = np.column_stack(vectors)
+    return np.mean(c,axis=1)
+
 
 class Plant(object):
     """plant composed of nodes"""
@@ -28,6 +42,18 @@ class Plant(object):
         self.mesh_object = mesh_helpers.init_mesh_object()
         self.bbox_lower = np.array((0.,0.,0.)) 
         self.bbox_upper = np.array((0.,0.,0.))
+        self._init_plant_shape()
+
+    def _init_plant_shape(self):
+        '''
+        add custom initial geometry to plant
+        '''
+        idx = 0
+        start_vec = np.array((0.0,0.0,1.3))
+        parent = self.get_node(idx)
+        loc = parent.location + start_vec
+        new_node = Node(parent=idx,coordinates=loc)
+        self._add_node(new_node)
 
     def number_of_elements(self):
         return len(self.nodes)
@@ -44,8 +70,8 @@ class Plant(object):
             if not collided:
                 continue
             else:
-                pos_vec,index,dist = collided[0]
-                new_node = Node(index,p.position)
+                pos_of_node,index,dist = collided[0]
+                new_node = self.spawn_new_node(p.position,index,dist)
                 self._add_node(new_node)
 
     def _create_spatial_tree(self):
@@ -54,7 +80,44 @@ class Plant(object):
             spatial_tree.insert(node.location,i)
         spatial_tree.balance()
         return spatial_tree
+
+    def spawn_new_node(self,pos_vec,node_idx,dist):
+        '''
+        create new node
+        pos_vec = position of sphere
+        node_idx = parent node
+        dist = distance from parent node to sphere
+        '''
+        location = self._get_new_node_position(pos_vec,node_idx)
+        return Node(parent=node_idx,coordinates=location)
     
+    def _get_new_node_position(self,pos_vec,node_idx):
+        '''
+        NOTe: should be renamed to get average between internode 
+        and sphere position
+        determine where the new node should be
+        given the position of the sphere, the node it intersected
+        and its distance to that node
+        '''
+        parent_node = self.get_node(node_idx)
+        internode_vec = self.get_parent_internode_vec(node_idx)
+        node_to_sphere = np.array(pos_vec)-parent_node.location
+        displacement = get_average_vector((internode_vec,node_to_sphere))
+        return displacement + parent_node.location
+
+
+    def get_parent_internode_vec(self,node_idx):
+        '''
+        gets vector from parent to input node
+        '''
+        node = self.get_node(node_idx)
+        to_vec = node.location
+        from_vec = self._get_parent_node(node).location
+        return to_vec-from_vec
+
+    def get_node(self,node_idx):
+        return self.nodes[node_idx]
+
     def _add_node(self,new_node):
         self.nodes.append(new_node)
         self._update_bbox(new_node.location)
@@ -64,7 +127,7 @@ class Plant(object):
         self.bbox_upper = np.maximum(self.bbox_upper,test_location)
 
     def _get_parent_node(self,node):
-        return self.nodes[node.parent]
+        return self.get_node(node.parent)
 
     def show(self):
         #for some reason all geometry appears to be parented together in blender
--- test_plant.py
+++ test_plant.py
@@ -1,6 +1,12 @@
-import unittest,imp
+import bpy
+import sys,os,imp
+loc = os.path.dirname(bpy.data.filepath)
+if not loc in sys.path:
+    sys.path.append(loc)
+import unittest
 import numpy as np
 import mathutils
+
 import plant
 imp.reload(plant)
 
@@ -35,5 +41,19 @@ class PlantTestCase(unittest.TestCase):
         np.testing.assert_equal(self.plant.bbox_lower,(-1.,0.,0.))
         np.testing.assert_equal(self.plant.bbox_upper,(1.,10.,1.))
 
+    def test_get_parent_internode_vec(self):
+        new_node = plant.Node(0,(1,1,1))    
+        self.plant._add_node(new_node)
+        vec = self.plant.get_parent_internode_vec(1)
+        np.testing.assert_equal(vec,(1.,1.,1.)) 
+
+    def _test_get_new_node_position(self):
+        #Test out of date!
+        new_node = plant.Node(0,(1,1,1))    
+        self.plant._add_node(new_node)
+        p_pos = (2.,2.,0.)
+        vec = self.plant._get_new_node_position(p_pos,node_idx=1)
+        np.testing.assert_equal(vec,(1.5,1.5,.5))
+        
 if __name__=="__main__":
     unittest.main(exit=False)
