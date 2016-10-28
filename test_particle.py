48fc857f04d1f7c99bb1590e5066b9988f6bbbcf
JOSHUA_LOPEZ_BINDER <joshlobinder@gmail.com>
2016-10-28 11:31:27 -0700

    added test for random vector

--- test_particle.py
+++ test_particle.py
@@ -1,4 +1,4 @@
-import unittest,imp
+import unittest,imp,random
 import nutrients
 import numpy as np
 imp.reload(nutrients)
@@ -9,16 +9,21 @@ class ParticleTestCase(unittest.TestCase):
         pos = (0.0,0.0,0.0)
         radius = .7
         trend_velocity = (0.0,0.0,-1.0)
-        self.p = nutrients.Particle(pos,radius,trend_velocity)
+        self.p = nutrients.Particle(pos,radius)
 
     def test_move(self):
         original_position = np.array(self.p.position)
-        self.p.move()
+        self.p.move(magnitude=1.0)
         moved_position = np.array(self.p.position)
         self.assertFalse(np.array_equal(original_position,moved_position))
 
     def test_shows(self):
         self.p.show()
+
+    def test_get_random_vector(self):
+        test_number = random.uniform(0.0,2000.)
+        vec = self.p._get_random_vector(magnitude=test_number)
+        self.assertAlmostEqual(np.linalg.norm(vec),test_number,places=10)
 
 if __name__=="__main__":
     unittest.main(exit=False)
