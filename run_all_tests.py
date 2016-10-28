ad0a3f1217901230da4f8be2b7937f2a1ec67225
JOSHUA_LOPEZ_BINDER <joshlobinder@gmail.com>
2016-10-28 11:30:46 -0700

    removed unncessary blender stuff

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,8 +1,11 @@
 import unittest
-import bpy
-import sys,os
-loc = os.path.dirname(bpy.data.filepath)
+#import bpy
+#import sys,os
+#loc = os.path.dirname(bpy.data.filepath)
+loc = '.'   
 
-loader = unittest.defaultTestLoader
-suite = loader.discover(start_dir=loc,pattern='test*.py')
-unittest.TextTestRunner(verbosity=2).run(suite)
+
+if __name__=="__main__":
+    loader = unittest.TestLoader()
+    suite = loader.discover(start_dir=loc,pattern='test*.py',top_level_dir=loc)
+    unittest.TextTestRunner(verbosity=2).run(suite)
