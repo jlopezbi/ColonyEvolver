88d33bac4e16520ba3e196baf27543a65a39422f
JOSHUA_LOPEZ_BINDER <joshlobinder@gmail.com>
2017-03-23 15:36:30 -0700

    removed mem-leak behavior: new grower instance each simulation run

--- evolve_plants.py
+++ evolve_plants.py
@@ -6,10 +6,12 @@ copied from
 https://github.com/DEAP/deap/blob/a90d3d599aa789a0083f5bc299803ec32d491cbd/examples/gp/symbreg.py
 '''
 
+from __future__ import print_function
 import sys, imp
 import deap.gp as gp
 import pygraphviz as pgv
 import numpy as np
+import time
 #import vector_operations
 import math, random, operator
 import uuid
@@ -36,38 +38,47 @@ creator.create("Individual", gp.PrimitiveTree, fitness=creator.FitnessMax)
 
 ''' toolbox '''
 toolbox = base.Toolbox()
-toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=2)
+toolbox.register("expr", gp.genHalfAndHalf, pset=pset, min_=1, max_=5)
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.expr)
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 toolbox.register("compile", gp.compile, pset=pset)
 
-
-''' fitness evaulator '''
-runner = grower.Grower.create_default_grower()
-def evalPhenotype(genome,runs):
-    '''
-    NOte: must return a tuple value!
-    '''
-    target_number_nodes = 13.0
+def make_phenotype(genome):
+    runner = grower.Grower.create_default_grower()
     func = toolbox.compile(expr=genome)
     seed = grower.seed_stem(func)
+    return runner.grow(seed,t_steps=20)
+
+''' fitness evaulator '''
+def evalPhenotype(genome,runs):
+    '''
+    Note: must return a tuple value!
+    '''
+    target_number_nodes = 13.0
     fitness_vals = []
+    #start_time = time.time()
     for run in range(runs):
-        # assign a fitness to the individual
-        phenotype = runner.grow(seed,t_steps=20)
-        health = phenotype.get_health()
+        phenotype = make_phenotype(genome)
+        #health = phenotype.get_health()
         size = phenotype.number_of_elements()
         fitness = math.fabs(target_number_nodes-size)
         fitness_vals.append(fitness)
-    # do some sort of combining of the fitness
+        #print(".", sep='', end='')
+    #end_time = time.time()
+    #elapsed = end_time - start_time
+    #print("t: "+str(elapsed))
     return summarize_values(fitness_vals)
 
 def summarize_values(values):
     return np.average(values),
-
-POP_SIZE = 20
-PHENO_RUNS = 2
-N_GEN = 2
+#############################################################
+## PARAMETERS
+POP_SIZE = 40
+PHENO_RUNS = 4
+N_GEN = 10
+max_size = 13 #of processor tree
+prob_mate = 0.5
+prob_mutate = 0.1
 
 toolbox.register("evaluate", evalPhenotype, runs=PHENO_RUNS)
 toolbox.register("select", tools.selTournament, tournsize=3)
@@ -75,8 +86,8 @@ toolbox.register("mate", gp.cxOnePoint)
 toolbox.register("expr_mut", gp.genFull, min_=1, max_=7)
 toolbox.register("mutate", gp.mutUniform, expr=toolbox.expr_mut, pset=pset)
 
-toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
-toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=17))
+toolbox.decorate("mate", gp.staticLimit(key=operator.attrgetter("height"), max_value=max_size))
+toolbox.decorate("mutate", gp.staticLimit(key=operator.attrgetter("height"), max_value=max_size))
 
 ''' Add Genealogy recorder '''
 history = tools.History()
@@ -108,7 +119,7 @@ def main():
     mstats.register("min", np.min)
     mstats.register("max", np.max)
 
-    pop, log = algorithms.eaSimple(pop, toolbox, 0.5, 0.1, N_GEN, stats=mstats,
+    pop, log = algorithms.eaSimple(pop, toolbox, prob_mate, prob_mutate, N_GEN, stats=mstats,
                                    halloffame=hof, verbose=True)
     # print log
     
